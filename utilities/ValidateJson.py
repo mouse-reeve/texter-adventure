37d8f0410802bbc18f88866033183b758c816c16
Mouse Reeve <mouse.reeve@gmail.com>
2015-06-29 15:45:56 -0700

    Uses logging for validator

--- utilities/ValidateJson.py
+++ utilities/ValidateJson.py
@@ -1,5 +1,6 @@
 ''' Checks if the game json file will produce a valid graph '''
 import json
+import logging
 import sys
 
 def validate(turns):
@@ -7,26 +8,28 @@ def validate(turns):
     turn_uids = []
     for turn in turns:
         if not ('id' in turn and 'text' in turn and 'options' in turn):
-            return 'fields missing in turn %s' % json.dumps(turn)
+            logging.error('fields missing in turn %s', json.dumps(turn))
 
         if not isinstance(turn['text'], list):
-            return 'text should be a list for turn %s' % json.dumps(turn)
+            logging.error('text should be a list for turn %s', json.dumps(turn))
 
         if turn['id'] in turn_uids:
-            return 'duplicate turn uid %s' % turn['id']
+            logging.error('duplicate turn uid %s', turn['id'])
+
+        for text in turn['text']:
+            if len(text) > 140:
+                logging.warn('Turn %s too long', turn['id'])
 
         turn_uids.append(turn['id'])
 
     for turn in FILE:
         for option in turn['options']:
             if not ('text' in option and 'destination' in option):
-                return 'fields missing from option %s' % json.dumps(turn)
+                logging.error('fields missing from option %s', json.dumps(turn))
 
             if not option['destination'] in turn_uids:
-                return 'unknown destination %s in turn %s' % (json.dumps(option), turn['id'])
-
-    return 'SUCCESS: valid file'
+                logging.error('unknown destination %s in turn %s', json.dumps(option), turn['id'])
 
 if __name__ == '__main__':
     FILE = json.load(open(sys.argv[1], 'r'))
-    print validate(FILE)
+    validate(FILE)
