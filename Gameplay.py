f762370554daba783d3b8c67a9aba47d6a60f3bc
Mouse Reeve <mouse.reeve@gmail.com>
2015-07-07 17:56:04 -0700

    Updates functionality for new source format

--- Gameplay.py
+++ Gameplay.py
@@ -9,44 +9,63 @@ from IO import SysIO
 
 def start():
     ''' initualize and accesses the first turn in a game '''
-    automatic_turn('START')
+    automatic_turn(0)
 
 
 def automatic_turn(uid):
     ''' gets a turn within the known graph '''
-    turns = GRAPH.cypher.execute('MATCH (t:turn) WHERE t.uid = "%s" RETURN t' % uid)
-    options = GRAPH.cypher.execute('MATCH (t:turn) --> (o:option) '
-                                   'WHERE t.uid = "%s" '
-                                   'RETURN o' % uid)
-    turn_node = turns[0][0]
+
+    text = []
+
+    while True:
+        current = GRAPH.cypher.execute('MATCH (t:turn) WHERE t.uid = %s RETURN t' % uid)
+        text.append(current[0][0]['text'])
+        try:
+            uid = GRAPH.cypher.execute('MATCH t --> (t2:turn) ' \
+                                       'WHERE t.uid = %s RETURN t2' % uid)[0][0]['uid']
+        except:
+            break
+
+    prompt = GRAPH.cypher.execute('MATCH (t:turn) --> (p:prompt) '
+                                  'WHERE t.uid = %s '
+                                  'RETURN p' % uid)
+    try:
+        prompt = prompt[0][0]
+        options = GRAPH.cypher.execute('MATCH (p:prompt) --> (o:option) '
+                                       'WHERE p.uid = %s '
+                                       'RETURN o' % prompt['uid'])
+    except:
+        prompt = {'text': ''}
+        options = []
+
     optionset = []
     for option in options:
         option = option[0]
         optionset.insert(0, option)
 
-    turn(turn_node['text'], optionset)
+    turn(text, prompt['text'], optionset)
 
 
 def custom_turn(turn_data):
     ''' runs a custom, on-the-fly generated turn '''
-    turn(turn_data['text'], turn_data['options'])
+    turn(turn_data['text'], turn_data['prompt'], turn_data['options'])
 
 
-def turn(texts, options):
+def turn(texts, prompt, options):
     ''' runs a turn '''
 
     for text in texts:
-        LOGGER.info('sending message: %s' % text)
+        LOGGER.info('sending message: %s', text)
         send_message(text)
 
     if len(options):
-        options_text = format_options(options)
-        LOGGER.info('sending message: %s' % options_text)
+        options_text = format_options(prompt, options)
+        LOGGER.info('sending message: %s', options_text)
         send_message(options_text)
 
         selection = pick_option(options)
         if selection:
-            automatic_turn(selection['destination'])
+            automatic_turn(selection['pointsTo'][0])
         else:
             selection = custom_response()
             custom_turn(selection)
@@ -60,9 +79,9 @@ def send_message(message):
         LOGGER.error('Failed to send message')
 
 
-def format_options(options):
+def format_options(prompt, options):
     ''' creates the options menu formatted string '''
-    message = ['Please select an option: ']
+    message = [prompt]
     for (i, option) in enumerate(options):
         message.append('%s) %s ' % (chr(ord('A')+i), option['text']))
     return '\n'.join(message)
