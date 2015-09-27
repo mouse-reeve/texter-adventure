c03b9f615334547407ff91eb022c1b9c837d8ab1
Mouse Reeve <mouse.reeve@gmail.com>
2015-09-27 15:41:08 -0700

    Option to remove options

--- static/js/main.controller.js
+++ static/js/main.controller.js
@@ -58,6 +58,11 @@ function MainController($scope, Game) {
         Game.hide(game.phone);
     };
 
+    $scope.removeOptions = function(phone) {
+        delete $scope.turn[phone].prompt;
+        $scope.turn[phone].options = null;
+    };
+
     $scope.checkOptionLength = function(turn) {
         var total = turn.prompt.length;
         angular.forEach(turn.options, function(option) {
