ccf470510bb57ad72f68e0059f300d3dacb3c361
Mouse Reeve <mouse.reeve@gmail.com>
2015-09-29 14:46:57 -0700

    Adds http interceptor for API response

--- static/js/app.js
+++ static/js/app.js
@@ -1,15 +1,32 @@
-angular.module('app', ['gameFactory', 'ngRoute'])
-    .config(function ($locationProvider, $routeProvider) {
+angular.module('app', ['gameFactory', 'ngRoute']).config(function ($httpProvider, $locationProvider, $routeProvider) {
+    $routeProvider
+        .when('/', {
+            controller: 'MainController',
+            templateUrl: 'static/partials/main.html'
+        })
+        .otherwise({
+            redirectTo: '/'
+        });
 
-        $routeProvider
-            .when('/', {
-                controller: 'MainController',
-                templateUrl: 'static/partials/main.html'
-            })
-            .otherwise({
-                redirectTo: '/'
-            });
+    $locationProvider.html5Mode(true);
 
-        $locationProvider.html5Mode(true);
+
+    $httpProvider.interceptors.push(function($q) {
+        return {
+            'response': function(response) {
+                var deferred = $q.defer();
+
+                if (response.config.url.startsWith('/api/')) {
+                    if (response.data.success) {
+                        deferred.resolve(response.data);
+                    } else {
+                        deferred.reject(response.data);
+                    }
+                    return deferred.promise;
+                }
+                return response;
+            }
+        };
     });
+});
 
--- static/js/game.factory.js
+++ static/js/game.factory.js
@@ -2,37 +2,37 @@ angular.module('gameFactory', []).factory('Game', function($http) {
     return {
         startNewGame: function() {
             return $http.get('/api/player/new').then(function(response) {
-                return response.data.data;
+                return response.data;
             });
         },
 
         addPlayer: function(name, phone) {
             return $http.post('/api/player', {'name': name, 'phone': phone}).then(function(response) {
-                return response.data.data;
+                return response.data;
             });
         },
 
         sendTurn: function(turnData, phone) {
             return $http.post('/api/message/' + phone, turnData).then(function(response) {
-                return response.data.data;
+                return response.data;
             });
         },
 
         getGames: function() {
             return $http.get('/api/player').then(function(response) {
-                return response.data.data;
+                return response.data;
             });
         },
 
         setUID: function(uid, phone){
             return $http.put('/api/player/' + phone + '/' + uid).then(function(response) {
-                return response.data.data;
+                return response.data;
             });
         },
 
         update: function(phone, changedData) {
             return $http.put('/api/player/' + phone, changedData).then(function(response) {
-                return response.data.data;
+                return response.data;
             });
         },
     };
